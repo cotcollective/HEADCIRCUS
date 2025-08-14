76ffff8f4ec4b94811e8ed37da96b2a8fd5210ed
Ubuntu <ubuntu@ip-172-31-38-87.us-east-2.compute.internal>
2025-08-14 23:18:56 +0000

    Version finale avec design cirque, outils et sons

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/dave/git/HEADCIRCUS/venv
+set -gx VIRTUAL_ENV /home/ubuntu/HEADCIRCUS/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
